e5af2622a6b3b85ad0eb973c880e8d6fed421708
Seth-Agyei <seth199299@gmail.com>
2026-09-19 22:15:40 -0700

    docker container created with 4 services

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+if [ ! -f .env]; then 
+    cp .env.example .env
+fi
+
+php artisan key:generate --force --ansi || true
+
+echo "waiting for datatbase..."
+until php -r "new PDO('pgsql:hodt=${DB_PORT};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}')" > /dev/null 2>&1; do
+    sleep 1
+done
+
+ech0 "Database is up.";
+
+php artisan migrate --force
+php artisan db:seed --force || true
+
+exec php-fpm
