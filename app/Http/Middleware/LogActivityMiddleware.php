e3612d38a02d83e4eddef98b1939b8432a90b21f
Seth-Agyei <seth199299@gmail.com>
2026-09-21 14:57:10 -0700

    midlleware:LogActivity, Role--Created

---
+++ app/Http/Middleware/LogActivityMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\ActivityLog;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogActivityMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * recors every api for (who, where, when,how,what,result) for auditing
+     *
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        ActivityLog::create([
+            'user_id'=>$request->user()?->id,
+            'method'=>$request->method(),
+            'path'=>$request-> path(),
+            'action'=>$request->route()?->getActionMethod(),
+            'status_code'=>$request->getStatusCode(),
+            'ip_address'=>$request->ip(),
+        ]);
+
+        return $response
+    }
+}
